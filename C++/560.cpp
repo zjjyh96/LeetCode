edf8c07f769bec19542ec7b59a30e9610d0973d9
Yinhao Jiang <zjjyh96@126.com>
2018-10-02 00:12:55 -0700

    560. Subarray Sum Equals K

--- C++/560.cpp
+++ C++/560.cpp
@@ -9,6 +9,7 @@ using namespace std;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+
         int n=nums.size();
         int count=0;
         vector<int> sum(n+1);
@@ -26,4 +27,29 @@ public:
         }
         return count;
     }
+
+//    int subarraySum(vector<int>& nums, int k) {
+//
+//        int n=nums.size();
+//        int count=0;
+//        int sum=0;
+//        map<int,int> sumap;
+//        vector<int> s(n+1);
+//        s[0]=0;
+//
+//        for (int i=0;i<n;i++)
+//        {
+//            sumap[sum]++;
+//            sum += nums[i];
+//            s[i+1]=sum;
+//        }
+//        sumap[sum]++;
+//
+//        for (int i=0;i<n;i++)
+//        {
+//            count+=sumap[s[i]+k];
+//            sumap[s[i]]--;
+//        }
+//        return count;
+//    }
 };
