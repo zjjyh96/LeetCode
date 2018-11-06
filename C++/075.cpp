64804c7e3cf218df787d9173b1aef8eab0858b4e
Yinhao Jiang <zjjyh96@126.com>
2018-11-06 11:23:35 -0800

    75. Sort Colors

--- C++/075.cpp
+++ C++/075.cpp
@@ -9,7 +9,20 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
-
+        int n=nums.size();
+        int left=0, right=n-1;
+        int i=0;
+        while (i<=right)
+        {
+            if (nums[i]==0)
+            {
+                swap(nums[i++],nums[left++]);
+            }
+            else if (nums[i]==2)
+            {
+                swap(nums[i],nums[right--]);
+            }
+            else i++;
+        }
     }
 };
